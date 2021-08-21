aed82c8e1a7b815094d02fad41998fc86ae5650a
titov-andrei <titov_andrei@mail.ru>
2021-08-21 13:47:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(
-  React.createElement(
-    "button",
-    {
-      onClick: () => console.log('CLICK'),
-    },
-    "Нажми на меня!"
-  ),
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
