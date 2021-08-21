b9dca0cdc8d5f391e29d7416091d854c49189174
titov-andrei <titov_andrei@mail.ru>
2021-08-21 19:24:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./styles/App.css";
 
 function App() {
   return (
@@ -6,9 +7,7 @@ function App() {
       <div className="post">
         <div className="post__content">
           <strong>1. Javascript</strong>
-          <div>
-            Javascript - язык программирования
-          </div>
+          <div>Javascript - язык программирования</div>
         </div>
         <div className="post__btns">
           <button>Удалить</button>
