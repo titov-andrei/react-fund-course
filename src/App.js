0e98edf92cf65e749bd7c803acacc1052ec8be57
titov-andrei <titov_andrei@mail.ru>
2021-08-21 14:16:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  let likes = 0;
+  const [likes, setLikes] = useState(0);
+  function inc() {
+    setLikes(likes + 1);
+  }
+  function dec() {
+    setLikes(likes - 1);
+  }
   return (
     <div className="App">
       <h1>{likes}</h1>
-      <button onClick={() => (likes += 1)}>Inc</button>
-      <button onClick={() => (likes -= 1)}>Dec</button>
+      <button onClick={inc}>Inc</button>
+      <button onClick={dec}>Dec</button>
     </div>
   );
 }
