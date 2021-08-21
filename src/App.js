2e7b8e867b2cd19dfa6a4199b5c510b49e323107
titov-andrei <titov_andrei@mail.ru>
2021-08-21 18:42:57 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 
 function App() {
-  const [likes, setLikes] = useState(0);
-  function inc() {
-    setLikes(likes + 1);
-  }
-  function dec() {
-    setLikes(likes - 1);
-  }
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <button onClick={inc}>Inc</button>
-      <button onClick={dec}>Dec</button>
+      <div className="post">
+        <div className="post__content">
+          <strong>1. Javascript</strong>
+          <div>
+            Javascript - язык программирования
+          </div>
+        </div>
+        <div className="post__btns">
+          <button>Удалить</button>
+        </div>
+      </div>
     </div>
   );
 }
