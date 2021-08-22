b1aefb5829626277bd4841ec8bb2514ddfdbcdcc
titov-andrei <titov_andrei@mail.ru>
2021-08-22 09:56:25 +0300

    Update PostList.jsx

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import PostItem from "./PostItem";
 
-const PostList = ({posts}) => {
+const PostList = ({posts, title}) => {
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>Список постов</h1>
+      <h1 style={{ textAlign: "center" }}>{title}</h1>
       {posts.map((post) => (
         <PostItem post={post} key={post.id} />
       ))}
