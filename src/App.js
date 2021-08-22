ccc64e72439447dc28240f11e770794ea74608f2
titov-andrei <titov_andrei@mail.ru>
2021-08-22 08:38:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
+import PostItem from "./components/PostItem";
 import "./styles/App.css";
 
 function App() {
+  const [posts, setPosts] = useState(
+    [
+      {id: 1, title: 'Javascript', body: 'Description'},
+      {id: 2, title: 'Javascript 2', body: 'Description'},
+      {id: 3, title: 'Javascript 3', body: 'Description'}
+    ]
+  )
   return (
     <div className="App">
-      <div className="post">
-        <div className="post__content">
-          <strong>1. Javascript</strong>
-          <div>Javascript - язык программирования</div>
-        </div>
-        <div className="post__btns">
-          <button>Удалить</button>
-        </div>
-      </div>
+      <h1>Список постов</h1>
+      {posts.map((post) => 
+        <PostItem post={post} key={post.id} />
+      )}
     </div>
   );
 }
